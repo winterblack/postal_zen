abb4e9fc84d9009187c2863946012ab1c50dd299
Wylliam Judd <wylliamjudd@gmail.com>
2017-01-30 13:33:32 -0800

    add method to upload images to froala image manager

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
 
+  # Froala Image Manager API response
+  get 'image_manager' => 'image_manager#index'
+
   # Spree Routes
   mount Spree::Core::Engine, :at => '/shop'
   Spree::Core::Engine.routes.draw do
