c5d5b263308daa527998c24462e57e2e868bbcd0
Wylliam Judd <wylliamjudd@gmail.com>
2017-03-21 10:23:32 -0700

    fix transactional emails

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,7 +14,7 @@ Spree.config do |config|
   config.currency = "USD"
 
   # from address for transactional emails
-  config.mails_from = "store@example.com"
+  config.mails_from = "contactus@postalzen.com"
 
   # Uncomment to stop tracking inventory levels in the application
   config.track_inventory_levels = false
